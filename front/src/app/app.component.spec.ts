a8fc340567cc9254962c4358b0d6c874b07e68f3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-06 13:44:25 +0200

    unit test ok

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front'`, () => {
+  it(`should have as name 'Jean-Louis'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front');
+    expect(app.name).toEqual('Jean-Louis');
   });
 
-  it('should render title', () => {
+  it('should render name', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('front app is running!');
+    expect(compiled.querySelector('h1 span').textContent).toContain('Jean-Louis');
   });
 });
