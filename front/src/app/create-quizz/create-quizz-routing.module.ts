931a23d2ab1c423380c8cc61224137359ddac25c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-07 15:46:38 +0200

    added dom

--- front/src/app/create-quizz/create-quizz-routing.module.ts
+++ front/src/app/create-quizz/create-quizz-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddQuestionComponent } from './add-question/add-question.component';
 
 import { CreateQuizzComponent } from './create-quizz.component';
+import { FinishedComponent } from './finished/finished.component';
+import { SetupComponent } from './setup/setup.component';
 
-const routes: Routes = [{ path: '', component: CreateQuizzComponent }];
+const routes: Routes = [
+  { path: '', component: CreateQuizzComponent },
+  { path: 'setup', component: SetupComponent },
+  { path: 'add-question', component: AddQuestionComponent },
+  { path: 'finished', component: FinishedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- front/src/app/create-quizz/setup/setup.component.ts
+++ front/src/app/create-quizz/setup/setup.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-setup',
   templateUrl: './setup.component.html',
-  styleUrls: ['./setup.component.scss']
+  styleUrls: ['./setup.component.scss'],
 })
 export class SetupComponent implements OnInit {
+  name = `Les capitale d'Europe`;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
