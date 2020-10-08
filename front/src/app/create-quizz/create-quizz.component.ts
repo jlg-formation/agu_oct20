40bba277ae46ed8205cd4f8735e23291835c3dae
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-08 11:52:06 +0200

    added router

--- front/src/app/create-quizz/create-quizz.component.ts
+++ front/src/app/create-quizz/create-quizz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-quizz',
@@ -10,7 +11,12 @@ export class CreateQuizzComponent implements OnInit {
   f = new FormGroup({
     name: new FormControl(`Les capitales d'Europe`, [Validators.required]),
   });
-  constructor() {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {}
+
+  submit(): void {
+    console.log('submit');
+    this.router.navigate(['./setup'], { relativeTo: this.route });
+  }
 }
