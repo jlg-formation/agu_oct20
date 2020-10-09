cac259c33a54c00122adb8ee527b17de8133c70e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-09 14:22:34 +0200

    ng test coverage question

--- front/src/app/do-quizz/question/question.component.spec.ts
+++ front/src/app/do-quizz/question/question.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Quizz } from 'src/app/interfaces/quizz';
+import { QuizzProgress } from 'src/app/interfaces/quizz-progress';
+import { QuizzService } from 'src/app/quizz.service';
 
 import { QuestionComponent } from './question.component';
 
 describe('QuestionComponent', () => {
   let component: QuestionComponent;
+  let service: QuizzService;
   let fixture: ComponentFixture<QuestionComponent>;
 
   beforeEach(async () => {
@@ -12,15 +16,37 @@ describe('QuestionComponent', () => {
       imports: [RouterTestingModule],
       declarations: [QuestionComponent],
     }).compileComponents();
+    service = TestBed.inject(QuizzService);
+    service.current = {
+      name: 'toto',
+      questions: [
+        { text: 'qqq', answers: ['', '', '', ''], correctAnswer: 'C' },
+      ],
+    } as Quizz;
+    service.progress = {
+      score: 0,
+      currentQuestion: 0,
+    } as QuizzProgress;
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(QuestionComponent);
     component = fixture.componentInstance;
+    component.nbr = 1;
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    service.current = undefined;
+    service.progress = undefined;
+  });
+
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call submit', () => {
+    component.submit();
     expect(component).toBeTruthy();
   });
 });
