178f2ba88cfde30fa1f2baee1d2df15cd931737b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-09 09:24:40 +0200

    200 ok on /ws/quizz

--- back/server.ts
+++ back/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import serveIndex from "serve-index";
 import cors from "cors";
+import { QuizzMap } from "../front/src/app/interfaces/quizz-map";
 
 const app = express();
 const port = 3000;
-const www = '../front/dist/front';
+const www = "../front/dist/front";
+
+const map: QuizzMap = {};
 
 app.use((req, res, next) => {
   console.log("req.url", req.url);
@@ -12,9 +15,14 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
+
+app.get("/ws/quizz", (req, res) => {
+  res.json(map);
+});
+
 app.use(express.static(www));
 app.use(serveIndex(www, { icons: true }));
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
