812c11f5a168bcf561b2e75a90e7c2df82f8895b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-09 11:51:21 +0200

    ng e2e completed

--- front/e2e/src/po/add-question.po.ts
+++ front/e2e/src/po/add-question.po.ts
@@ -3,18 +3,14 @@ import { Question } from 'src/app/interfaces/question';
 
 export class AddQuestionPage {
   async submit(): Promise<void> {
-    await browser.sleep(1000);
     await element(by.cssContainingText('button', 'Ajouter')).click();
-    await browser.sleep(1000);
   }
 
   async input(question: Question): Promise<void> {
-    await browser.sleep(1000);
     const textarea = element(by.css('textarea'));
     await textarea.clear();
     await textarea.sendKeys(question.text);
     for (const i of [0, 1, 2, 3]) {
-      await browser.sleep(1000);
       const inp = element(by.css(`.first-col input[ng-reflect-name="${i}"]`));
       await inp.clear();
       await inp.sendKeys(question.answers[i]);
--- front/e2e/src/po/finished.po.ts
+++ front/e2e/src/po/finished.po.ts
@@ -2,8 +2,6 @@ import { browser, by, element } from 'protractor';
 
 export class FinishedPage {
   async clickHomeBtn(): Promise<void> {
-    await browser.sleep(1000);
-
     await element(by.css('button')).click();
   }
 }
